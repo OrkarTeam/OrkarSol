7dfbfc61801129e92ced1024da736cf3643f0745
George-Olufemi <georgeoluolufemi@gmail.com>
2023-04-29 11:21:17 +0100

    finished connect wallet modal desktop

---
+++ app/data/static/wallets-details.js
@@ -0,0 +1,31 @@
+import metamask from '@/app/assets/images/metamask.svg';
+import trustwallet from '@/app/assets/images/trustwallet.svg';
+import walletconnect from '@/app/assets/images/walletconnect.svg';
+import arrow from '@/app/assets/images/arrow.svg'
+
+export const walletDetails = [
+    {
+        id: 1,
+        name: 'metamask',
+        logo: metamask,
+        end: arrow
+    },
+    {
+        id: 2,
+        name: 'trust wallet',
+        logo: trustwallet,
+        end: arrow
+    },
+    {
+        id: 3,
+        name: 'wallet connect',
+        logo: walletconnect,
+        end: arrow
+    },
+    {
+        id: 4,
+        name: 'coinbase',
+        logo: walletconnect,
+        end: arrow
+    }
+]
--- app/layout/Header/Navbar.js
+++ app/layout/Header/Navbar.js
@@ -10,8 +10,9 @@ import searchicon from "@/app/assets/images/searchicon.svg";
 import WalletConnect from "@/app/components/walletconnect/WalletConnect";
 import routes from "@/app/config/routes.js";
 import WalletModal from "@/app/components/ui/walletmodal/Walletmodal";
+import { walletDetails } from "@/app/data/static/wallets-details";
 
-const Navbar = () => {
+const Navbar = ({ wallet }) => {
   const [walletModal, setWalletModal] = useState(false);
 
   // Function to handle button click and show the modal
@@ -22,6 +23,22 @@ const Navbar = () => {
   // Function to handle modal close and hide the modal
   const handleModalClose = () => {
     setWalletModal(false);
+  };
+
+  const handleMetamask = () => {
+    alert("metamask");
+  };
+
+  const handleTrustWallet = () => {
+    alert("trustwallet");
+  };
+
+  const handleWalletConnect = () => {
+    alert("wallet connect");
+  };
+
+  const handleCoinbase = () => {
+    alert("coinbase");
   };
 
   return (
@@ -91,9 +108,33 @@ const Navbar = () => {
       </div>
       {walletModal && ( // Render the modal only if showModal is true
         <WalletModal onClose={handleModalClose}>
-        <div>
-          <h2 className="Satoshil font-[550] text-[28.02px] leading-[38px] text-white pb-[18.04px]">Connect Wallet</h2>
-          <p className="text-[#C2C2C2] font-[400] text-[28.4176px] leading-[30px]">If you don't have a wallet yet, you can select a provider and create one now.</p>
+          <div className="pb-[76.48px]">
+            <h2 className="Satoshil font-[550] text-[28.02px] leading-[38px] text-white pb-[18.04px]">
+              Connect Wallet
+            </h2>
+            <p className="text-[#C2C2C2] font-[400] text-[22.4176px] leading-[30px]">
+              If you don't have a wallet yet, you can select a provider and
+              create one now.
+            </p>
+          </div>
+          <div>
+            {walletDetails.map((wallet, index) => (
+              <Item
+                key={index}
+                data={wallet}
+                onClick={() => {
+                  if (index === 0) {
+                    handleMetamask();
+                  } else if (index === 1) {
+                    handleTrustWallet();
+                  } else if (index === 2) {
+                    handleWalletConnect();
+                  } else if (index === 3) {
+                    handleCoinbase();
+                  }
+                }}
+              />
+            ))}
           </div>
         </WalletModal>
       )}
@@ -102,3 +143,24 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+const Item = ({ data, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={clsx([
+        "item px-[28.02px] py-[22.42px] flex justify-between items-center cursor-pointer mt-4 mb-4 hover:bg-[#1F1F1F] transition-all duration-500",
+      ])}
+    >
+      <div className="flex items-center gap-4">
+        <div>
+          <Image src={data.logo} width="" height="" alt="" />
+        </div>
+        <div className="capitalize">{data.name}</div>
+      </div>
+      <div>
+        <Image src={data.end} width="" height="" alt="" />
+      </div>
+    </div>
+  );
+};
