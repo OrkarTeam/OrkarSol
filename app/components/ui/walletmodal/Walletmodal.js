05c6346848682af457c631fb97c67040aa10e7b0
George-Olufemi <georgeoluolufemi@gmail.com>
2023-04-29 11:29:34 +0100

    null

--- app/components/ui/walletmodal/Walletmodal.js
+++ app/components/ui/walletmodal/Walletmodal.js
@@ -7,10 +7,13 @@ const WalletModal = ({ onClose, children }) => {
   useEffect(() => {
     const handleClick = (event) => {
       const modal = modalRef.current;
+
+      // Close the modal if the clicked element is not a child of the modal
       if (modal && !modal.contains(event.target)) {
         onClose();
       }
     };
+
     document.addEventListener("click", handleClick);
     return () => {
       document.removeEventListener("click", handleClick);
@@ -25,7 +28,7 @@ const WalletModal = ({ onClose, children }) => {
     <div
       onClick={handleModalClick}
       ref={modalRef}
-      className="bg-[#151515] h-[767.8px] w-[1020px] absolute top-[30px] modal transition-all duration-500 px-[56px] py-[97.14px]"
+      className="bg-[#151515] px-[56.04px] py-[97.14px] h-[700px] w-[1020px] absolute top-[12px] modal transition-all duration-500"
     >
       {/* <button onClick={onClose}>X</button> */}
       {children}
